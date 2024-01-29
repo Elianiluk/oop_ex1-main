0aac011a04493a814c4282e9534381167799095c
vboxuser <elianroy.iluk@msmail.ariel.ac.il>
2024-01-29 22:48:34 +0200

    w

---
+++ src/ConcreatePlayer.java
@@ -0,0 +1,29 @@
+package src.src;
+
+public class ConcreatePlayer implements Player{
+
+    public boolean isDefender;
+    private int winsCount;
+
+    public ConcreatePlayer(boolean isOne)
+    {
+        this.isDefender = isOne;
+        this.winsCount=0;
+    }
+
+    public boolean isPlayerOne()
+    {
+        return isDefender;
+    }
+
+    public void wins()
+    {
+        this.winsCount++;
+    }
+
+    @Override
+    public int getWins()
+    {
+        return this.winsCount;
+    }
+}
